82093842a647d895feea5c3f836416fec2f77d5e
htdoan27 <huynhthiendoan27@gmail.com>
2024-03-26 16:57:42 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -15,5 +13,11 @@ export default defineConfig({
   },
   server: {
     port: 3001,
-    },
+    proxy: {
+      "/api": {
+      target: "http://localhost:3000/",
+      changeOrigin: true,
+      },
+      }    
+  },
 })
